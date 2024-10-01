529205a0918f3e9a214a15eb147f21f643c75c5d
fen9z <fffzf077@gmail.com>
2024-09-30 20:37:28 -0400

    import product from product.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,5 @@
-import { cart as myCart } from "../data/cart.js";
-
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
 let productsHTML = "";
 
 products.forEach((product) => {
