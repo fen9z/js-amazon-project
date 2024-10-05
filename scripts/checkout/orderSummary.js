f335a556548268f11c00399e590788eef33130ad
fen9z <fffzf077@gmail.com>
2024-10-05 00:02:20 -0400

    create calculateDeliverDate function

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -12,6 +12,7 @@ import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import {
   deliveryOptions,
   getDeliveryOption,
+  calculateDeliverDate,
 } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
 import { renderCheckoutHeader } from "./checkoutHeader.js";
@@ -27,9 +28,7 @@ export function renderOrderSummary() {
 
     const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-    const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
-    const dateString = deliveryDate.format("dddd, MMMM D");
+    const dateString = calculateDeliverDate(deliveryOption);
 
     cartSummaryHTML += `
       <div class="cart-item-container 
@@ -85,9 +84,7 @@ export function renderOrderSummary() {
   function deliveryOptionsHTML(matchingProduct, cartItem) {
     let html = "";
     deliveryOptions.forEach((deliveryOption) => {
-      const today = dayjs();
-      const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
-      const dateString = deliveryDate.format("dddd, MMMM D");
+      const dateString = calculateDeliverDate(deliveryOption);
       const priceString =
         deliveryOption.priceCents === 0
           ? "FREE - "
