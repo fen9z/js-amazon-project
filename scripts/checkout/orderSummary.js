82e04c49ab7ba7daeb93236fea7df7458d303822
fen9z <fffzf077@gmail.com>
2024-10-04 22:38:08 -0400

    delete orderSummary some test code

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,9 +5,9 @@ import {
   updateQuantity,
   updateDeliveryOption,
 } from "../../data/cart.js";
-import { products, getProduct } from "../../data/products.js";
+import { getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
-import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
+
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import {
   deliveryOptions,
