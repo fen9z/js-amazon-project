4ac0a21eccbf069640153dabef016b5cd98f0fe2
fen9z <fffzf077@gmail.com>
2024-10-05 17:21:03 -0400

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -32,6 +32,7 @@ export function renderOrderSummary() {
 
     cartSummaryHTML += `
       <div class="cart-item-container 
+        js-cart-item-container
         js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">Delivery date: ${dateString}</div>
         <div class="cart-item-details-grid">
@@ -47,7 +48,9 @@ export function renderOrderSummary() {
             <div class="product-price">$${formatCurrency(
               matchingProduct.priceCents
             )}</div>
-            <div class="product-quantity">
+            <div class="product-quantity js-product-quantity-${
+              matchingProduct.id
+            }">
               <span> Quantity: <span class="quantity-label js-quantity-label-${
                 matchingProduct.id
               }">${cartItem.quantity}</span> </span>
@@ -62,9 +65,9 @@ export function renderOrderSummary() {
               <span class="save-quantity-link link-primary js-save-link" data-product-id="${
                 matchingProduct.id
               }">Save</span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+              <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${
                 matchingProduct.id
-              }">
+              }" data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
@@ -113,7 +116,7 @@ export function renderOrderSummary() {
     return html;
   }
 
-  document.querySelector(".order-summary").innerHTML = cartSummaryHTML;
+  document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
 
   document.querySelectorAll(".js-delete-link").forEach((link) => {
     link.addEventListener("click", () => {
