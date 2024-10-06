c7a4ec61f5cb46d07b89eb960a3957fdbbf94901
fen9z <fffzf077@gmail.com>
2024-10-06 13:22:49 -0400

    Learn more details about classes this

--- data/products.js
+++ data/products.js
@@ -54,6 +54,33 @@ class Clothing extends Product {
   }
 }
 
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+
+
+console.log(this);
+const object2 = {
+  a: 2,
+  b: this.a,
+};
+
+function logThis() {
+  console.log(this);
+}
+
+logThis();
+logThis.call("hello");
+
+const object3 = {
+  method: () => {
+    console.log(this);
+  },
+};
+object3.method();
+*/
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -531,5 +558,3 @@ export const products = [
   }
   return new Product(productDetails);
 });
-
-console.log(products);
