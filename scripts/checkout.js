6ee68efb80a2097c736807e3e08aeaec0fd71aaa
fen9z <fffzf077@gmail.com>
2024-10-07 22:17:52 -0400

    Convert from promises to asunc await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,6 +6,23 @@ import { loadCart } from "../data/cart.js";
 // import "../data/cart-class.js";
 // import "../data/backend-practice.js";
 
+async function loadPage() {
+  await loadProductsFetch();
+
+  const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve("value3");
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+}
+loadPage();
+
+/*
+
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -19,7 +36,7 @@ Promise.all([
   renderPaymentSummary();
   renderCheckoutHeader();
 });
-
+*/
 // new Promise((resolve) => {
 //   loadProducts(() => {
 //     resolve();
